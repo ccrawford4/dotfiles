70f6bd8312daac475f27631b9bf6108bd4079713
ccrawford4 <calumcrawford9@gmail.com>
2025-06-02 19:42:01 -0700

    update

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,10 @@
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
+vim.keymap.set("n", "<leader>o", ":put =''<CR>", { desc = "Insert line below", silent = true })
+vim.keymap.set("n", "<leader>O", ":put! =''<CR>", { desc = "Insert line above", silent = true })
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Escape terminal mode" })
